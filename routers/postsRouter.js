03ac35a1427b9843408e4989d0b9a2f0c71bf24e
Sara Salzano <sarasalzano95@gmail.com>
2025-10-17 13:52:13 +0200

    associa funzioni alle rotte

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,36 +2,26 @@
 const express = require("express");
 // creo un’istanza del router
 const router = express.Router();
+//import controller
+const postController = require("../controllers/postController");
 
 // index: restituisce la lista dei post
-router.get("/", function (req, res) {
-  res.send("Lista dei post");
-});
+router.get("/", postController.index);
 
 // show: restituisce i dettagli di un singolo post in base all'id
-router.get('/:id', function (req, res) {
-  res.send("Dettagli del post " + req.params.id);
-});
+router.get('/:id', postController.show);
 
 // store: crea un nuovo post
-router.post("/", function (req, res) {
-  res.send("Creazione nuovo post");
-});
+router.post("/", postController.store);
 
 // update: aggiorna un post esistente con PUT
-router.put("/:id", function (req, res) {
-  res.send("Modifica post " + req.params.id);
-});
+router.put("/:id", postController.update);
 
 // modify: modifica parziale di un post con PATCH
-router.patch("/:id", function (req, res) {
-  res.send('Modifica parziale del post ' + req.params.id);
-});
+router.patch("/:id", postController.modify);
 
 // destroy: elimina un post in base all'id
-router.delete("/:id", function (req, res) {
-  res.send("Eliminazione del post " + req.params.id);
-});
+router.delete("/:id", postController.destroy) 
 
 // esporta il router per poterlo usare in app.js
 module.exports = router;
