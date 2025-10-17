ee968b2b46077eea389a20302222c9db82b35ec6
Sara Salzano <sarasalzano95@gmail.com>
2025-10-17 13:36:38 +0200

    inserisci logica delle funzioni nel controller

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+//import dell array dei post
+const posts = require("../data/posts");
+
+//riporto logica delle funzioni che si trovano nel router. In questo caso la funzione index
+function index(req, res) {
+    res.send("Lista dei post");
+}
+
+//funzione show
+function show(req, res) {
+    res.send("Dettagli del post " + req.params.id);
+}
+
+//funzione store
+function store(req, res) {
+    res.send("Creazione nuovo post");
+}
+
+//funzione update
+function update(req, res) {
+    res.send("Modifica post " + req.params.id);   
+}
+
+//funzione modify 
+function modify(req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
+}
+
+//funzione delete 
+function del(req, res) {
+    res.send("Eliminazione del post " + req.params.id);
+}
+
+module.exports = {index, show, store, update, modify, del}; 
